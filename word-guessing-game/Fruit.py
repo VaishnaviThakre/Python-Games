4e4ebbf99855913590615a76afc88c59a5716050
Vaishnavi Thakre <86373178+VaishnaviThakre@users.noreply.github.com>
2024-01-10 13:25:26 +0530

    Add files via upload

---
+++ word-guessing-game/Fruit.py
@@ -0,0 +1,137 @@
+from tkinter import *
+from random import *
+from tkinter import messagebox
+import time
+
+FRUITS_WORD = ['NGMAO', 'EGNRAO', 'RRYCHE', 'ESPARG', 'LPAEP', 'RBREUEBYL', 'IWIK']
+
+FRUITS_ANSWER = ['MANGO', 'ORANGE', 'CHERRY',  'GRAPES', 'APPLE', 'BLUEBERRY', 'KIWI']
+
+ran_num = randrange(0, (len(FRUITS_WORD)))
+jumbled_rand_word = FRUITS_WORD[ran_num]
+
+points = 0
+
+
+def main():
+    def back():
+        my_window.destroy()
+        import main_start
+        main_start.start_main_page()
+
+    def change():
+        global ran_num
+        ran_num = randrange(0, (len(FRUITS_WORD)))
+        word.configure(text=FRUITS_WORD[ran_num])
+        get_input.delete(0, END)
+        ans_lab.configure(text="")
+
+    def check():
+        global points, ran_num
+        user_word = get_input.get().upper()
+        if user_word == FRUITS_ANSWER[ran_num]:
+            points += 5
+            score.configure(text="Score : " + str(points))
+            messagebox.showinfo('Correct', "That's Correct! Keep it up!")
+            ran_num = randrange(0, (len(FRUITS_WORD)))
+            word.configure(text=FRUITS_WORD[ran_num])
+            get_input.delete(0, END)
+            ans_lab.configure(text="")
+        else:
+            messagebox.showerror("Error", "That's Wrong! Try again!")
+            get_input.delete(0, END)
+
+    def show_answer():
+        global points
+        if points > 4:
+            points -= 5
+            score.configure(text="Score : " + str(points))
+            time.sleep(0.5)
+            ans_lab.configure(text=FRUITS_ANSWER[ran_num])
+        else:
+            ans_lab.configure(text='Not enough points')
+
+    my_window = Tk()
+    my_window.geometry("500x500+500+150")
+    my_window.resizable(0, 0)
+    my_window.title("Guess These Fruits Jumbled Words")
+    my_window.configure(background="#e6fff5")
+    #my_window.iconbitmap(r'quizee_logo_.ico')
+    #img1 = PhotoImage(file="back.png")
+
+    back_btn = Button(
+        text="Back",
+        width=8,
+        borderwidth=2,
+        bg='#e6fff5',
+        command=back,
+    )
+    back_btn.pack(anchor='nw', pady=10, padx=10)
+
+    score = Label(
+        text="Score:- 0",
+        pady=10,
+        bg="#e6fff5",
+        fg="#000000",
+        font="Titillium  14 bold"
+    )
+    score.pack(anchor="n")
+
+    word = Label(
+        text=jumbled_rand_word,
+        pady=10,
+        bg="#e6fff5",
+        fg="#000000",
+        font="Titillium  30 bold"
+    )
+    word.pack()
+
+    get_input = Entry(
+        font="none 26 bold",
+        borderwidth=10,
+        justify='center',
+    )
+    get_input.pack()
+
+    submit = Button(
+        text="Submit",
+        width=18,
+        borderwidth=8,
+        font=("", 13),
+        fg="#000000",
+        bg="#99ffd6",
+        command=check,
+    )
+    submit.pack(pady=(10,20))
+
+    change = Button(
+        text="Change Word",
+        width=18,
+        borderwidth=8,
+        fg="#000000",
+        bg="#99ffd6",
+        font=("", 13),
+        command=change,
+    )
+    change.pack()
+
+    ans = Button(
+        text="Answer",
+        width=18,
+        borderwidth=8,
+        fg="#000000",
+        bg="#99ffd6",
+        font=("", 13),
+        command=show_answer,
+    )
+    ans.pack(pady=(20, 10))
+
+    ans_lab = Label(
+        text="",
+        bg="#e6fff5",
+        fg="#000000",
+        font="Courier 15 bold",
+    )
+    ans_lab.pack()
+
+    my_window.mainloop()
